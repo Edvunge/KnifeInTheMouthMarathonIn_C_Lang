b8d37b7cfe1837250d52f83283a3e2605e9f97eb
edvunge <costaedy18@gmail.com>
2024-05-18 15:46:24 +0100

    w2 - variables_names

---
+++ week_02_VariablesAndConstts/book_C/variavelExemplo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 7, b = 4, soma;
+    
+    soma = a + b;
+    printf("%i", soma);
+
+    float temperatura1 = 17.30;
+    float temperatura2 = 22.56;
+    return (0);
+}
