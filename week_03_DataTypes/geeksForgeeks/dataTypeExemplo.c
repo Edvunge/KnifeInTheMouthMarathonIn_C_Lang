6413b8c7124060d3529b83eb2e93ff476d9dbb9e
edvunge <costaedy18@gmail.com>
2024-05-22 19:10:11 +0100

    w3 - update_exemple

---
+++ week_03_DataTypes/geeksForgeeks/dataTypeExemplo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    float altura = 1.2f;
+    double peso = 838.3;
+    char letra = 'S';
+    int idade = 98;
+
+    printf("altura: %.2f\n",altura);
+    printf("altura: %.2f\n",peso);
+    printf("altura: %c\n",letra);
+    printf("altura: %d\n",idade);
+    return (0);
+}
