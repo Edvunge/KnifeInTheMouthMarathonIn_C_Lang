5871ea4236b8af0f4b8b9395f76099f227fd764e
edvunge <costaedy18@gmail.com>
2024-05-29 23:15:33 +0100

    w4 - input

---
+++ week_04_Input_Output/GeeksForGeeks/printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+  
+// Driver code 
+int main() { 
+    // Declaring an int type variable 
+    int a; 
+  
+    // Assigning a value in a variable 
+    a = 20; 
+  
+    // Printing the value of a variable 
+    printf("%d", a); 
+  
+    return 0; 
+}
---
+++ week_04_Input_Output/GeeksForGeeks/simpleScanset.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    char str[128];
+
+    printf("Enter a string: ");
+    scanf("%[A-Z]s", str);
+
+    printf("You entered: %s\n", str);
+
+    return (0);
+}
